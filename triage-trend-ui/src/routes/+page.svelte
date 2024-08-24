43ea56d3b13511731524e409d52a51f270e58a7c
Mathis Kretz <mkretz@users.noreply.github.com>
2024-08-24 21:17:03 +0200

    Add details on main view

--- triage-trend-ui/src/routes/+page.svelte
+++ triage-trend-ui/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import type { Prediction } from '../lib/Prediction';
+	import type { Prediction } from '$lib/Prediction';
+	import type { Employee } from '$lib/Employee';
 	import { employees } from '$lib/Employee';
 
 	const startDate: string = (new Date()).toISOString().split('T')[0];
@@ -64,6 +65,16 @@
 		}
 	}
 
+	function teamSize(prediction: number): number {
+		if (prediction >= 15) {
+			return 5;
+		} else if (prediction < 15 && prediction >= 7) {
+			return 3;
+		} else {
+			return 2;
+		}
+	}
+
 	function dateText(date: string): string {
 		let dateObj = new Date(date);
 		const swissGermanDateText = dateObj.toLocaleDateString('de-CH', {
@@ -88,6 +99,33 @@
 		return shuffled.slice(0, count);
 	}
 
+	function publicHolidayCantons(date: string): string[] {
+		const prediction = predictionMap.get(date);
+		let cantons : string[] = [];
+		if (prediction?.featuresUsed.publicHolidayZurich === 1) {
+			cantons.push('ZH');
+		}
+		if (prediction?.featuresUsed.publicHolidayAargau === 1) {
+			cantons.push('AG');
+		}
+		if (prediction?.featuresUsed.publicHolidayZug === 1) {
+			cantons.push('ZG');
+		}
+		if (prediction?.featuresUsed.publicHolidaySchaffhausen === 1) {
+			cantons.push('SH');
+		}
+		if (prediction?.featuresUsed.publicHolidaySchwyz === 1) {
+			cantons.push('SZ');
+		}
+		if (prediction?.featuresUsed.publicHolidayStGallen === 1) {
+			cantons.push('SG');
+		}
+		if (prediction?.featuresUsed.publicHolidayThurgau === 1) {
+			cantons.push('TG');
+		}
+		return cantons;
+	}
+
 	loadData();
 </script>
 
@@ -96,16 +134,56 @@
 <div class="h-full mx-auto">
 	<div class="w-full min-h-[45vh] mb-7 bg-stone-200">
 		<div class="flex flex-row justify-between" data-name="container">
-			<div class="bg-amber-200 grow" data-name="left">
-				hoi
+			<div class="grow" data-name="left">
+				<div class="overflow-x-auto mx-10">
+				  <table class="table table-sm">
+				    <!-- head -->
+				    <thead>
+				      <tr>
+				        <th>Feature</th>
+				        <th>Wert</th>
+				      </tr>
+				    </thead>
+				    <tbody>
+				      <tr>
+				        <td>Kantone mit Feiertag</td>
+				        <td>{#each publicHolidayCantons(startDate) as canton}<span>{canton}</span>{/each}</td>
+				      </tr>
+				      <tr>
+				        <td>Regendauer</td>
+				        <td>{predictionMap.get(startDate)?.featuresUsed.totalRainDuration} Min.</td>
+				      </tr>
+				      <tr>
+				        <td>Temperatur (&#8960; 5 Tage)</td>
+				        <td>{predictionMap.get(startDate)?.featuresUsed.averageTemperature5dayMean} &deg;C</td>
+				      </tr>
+				      <tr>
+				        <td>Luftdruck</td>
+				        <td>{predictionMap.get(startDate)?.featuresUsed.averagePressure} hPa</td>
+				      </tr>
+				      <tr>
+				        <td>Luftdruck (&#8960; 5 Tage)</td>
+				        <td>{predictionMap.get(startDate)?.featuresUsed.averagePressure5dayMean} hPa</td>
+				      </tr>
+				      <tr>
+				        <td>Temperatur</td>
+				        <td>{predictionMap.get(startDate)?.featuresUsed.averageTemperature} &deg;C</td>
+				      </tr>
+				      <tr>
+				        <td>Mondphase</td>
+				        <td>{predictionMap.get(startDate)?.featuresUsed.moonPhase} <span class="material-symbols-outlined">circle</span></td>
+				      </tr>
+				    </tbody>
+				  </table>
+				</div>
 			</div>
-			<div class="bg-emerald-500 grow flex flex-col content-between" data-name="right">
-				<div class="bg-cyan-400 flex flex-row justify-start" data-name="prediction">
-					<div class="flex flex-col justify-start bg-purple-400" data-name="prediction-date">
-						<div class="text-center text-xl font-bold"
+			<div class="grow flex flex-col content-between p-5 pl-10 {predictionColor(roundedPrediction(startDate))}" data-name="right">
+				<div class="flex flex-row justify-start gap-4" data-name="prediction">
+					<div class="flex flex-col justify-start" data-name="prediction-date">
+						<div class="text-xl font-bold"
 							>{dateText(startDate).split(',')[0]}</div
 						>
-						<div class="text-center text-m"
+						<div class="text-m"
 							>{dateText(startDate).split(',')[1]}</div
 						>
 					</div>
@@ -118,12 +196,25 @@
 						</div>
 					</div>
 				</div>
-				<div class="bg-fuchsia-400 bg-cyan-400 flex flex-col content-between" data-name="team">
-					<div class="" data-name="team-title">
-						Einteilung
+				<div class="divider w-96"></div>
+				<div class="flex flex-col content-between" data-name="team">
+					<div class="mb-5" data-name="team-title">
+						<h1 class="font-bold text-xl">Triageteam</h1>
 					</div>
-					<div class="" data-name="team-cards">
-						cards
+					<div class="flex flex-row justify-start gap-4" data-name="team-cards">
+						{#each getRandomEmployees(teamSize(roundedPrediction(startDate))) as employee}
+							<div class="card bg-base-100 w-32 shadow-xl">
+								<figure class="h-32">
+							    <img
+							    	class="object-cover h-full w-full"
+							      src="{employee.imagePath}"
+							      alt="{employee.name}" />
+							  </figure>
+							  <div class="card-body px-5 py-2">
+							    <p>{employee.firstName} {employee.lastName}</p>
+							  </div>
+							</div>
+						{/each}						
 					</div>
 				</div>
 			</div>
