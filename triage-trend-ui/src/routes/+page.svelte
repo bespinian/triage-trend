5db085a7f9f56752c16de82be9af0576ebed0da1
Matthias Osswald <matthias@bespinian.io>
2024-08-24 22:18:03 +0200

    Add modal for future days details.

--- triage-trend-ui/src/routes/+page.svelte
+++ triage-trend-ui/src/routes/+page.svelte
@@ -147,6 +147,20 @@
     return MoonNew;
   }
 
+  function openModal(index) {
+    const modal = document.getElementById(`modal-${index}`);
+    if (modal) {
+      modal.showModal();
+    }
+  }
+
+  function closeModal(index) {
+    const modal = document.getElementById(`modal-${index}`);
+    if (modal) {
+      modal.close();
+    }
+  }
+
 	loadData();
 </script>
 
@@ -247,28 +261,143 @@
 							    <p>{employee.firstName} {employee.lastName}</p>
 							  </div>
 							</div>
-						{/each}						
+						{/each}
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 	<div class="container mx-auto grid grid-cols-7 gap-2">
+
 		{#each { length: 7 } as _, i}
-			<div class="card {predictionColor(roundedPrediction(offsetDate(startDate, i + 1)))}">
+			<div 
+				class="card {predictionColor(roundedPrediction(offsetDate(startDate, i + 1)))}" 
+				on:click={() => openModal(i)}
+			>
 				<div class="card-body">
-					<span class="text-center text-xl font-bold"
-						>{dateText(offsetDate(startDate, i + 1)).split(',')[0]}</span
-					>
-					<span class="text-center text-m"
-						>{dateText(offsetDate(startDate, i + 1)).split(',')[1]}</span
-					>
+					<span class="text-center text-xl font-bold">
+						{dateText(offsetDate(startDate, i + 1)).split(',')[0]}
+					</span>
+					<span class="text-center text-m">
+						{dateText(offsetDate(startDate, i + 1)).split(',')[1]}
+					</span>
 					<p class="text-6xl align-middle text-center">
 						{roundedPrediction(offsetDate(startDate, i + 1))}
 					</p>
 				</div>
 			</div>
+			<!-- Modal for each card -->
+			<dialog id={`modal-${i}`} class="modal">
+				<div class="modal-box w-11/12 max-w-5xl">
+					<div class="flex flex-row justify-between" data-name="container">
+						<div class="grow p-5" data-name="left">
+							<div class="overflow-x-auto mx-10">
+								<table class="table table-sm">
+									<thead>
+										<tr>
+											<th>Feature</th>
+											<th>Wert</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>Kantone mit Feiertag</td>
+											<td>
+												{#each publicHolidayCantons(offsetDate(startDate, i + 1)) as canton}
+													<span>{canton}</span>
+												{/each}
+											</td>
+										</tr>
+										<tr>
+											<td>Regendauer</td>
+											<td>{predictionMap.get(offsetDate(startDate, i + 1))?.featuresUsed.totalRainDuration} Min.</td>
+										</tr>
+										<tr>
+											<td>Temperatur (&#8960; 5 Tage)</td>
+											<td>{predictionMap.get(offsetDate(startDate, i + 1))?.featuresUsed.averageTemperature5dayMean} &deg;C</td>
+										</tr>
+										<tr>
+											<td>Luftdruck</td>
+											<td>{predictionMap.get(offsetDate(startDate, i + 1))?.featuresUsed.averagePressure} hPa</td>
+										</tr>
+										<tr>
+											<td>Luftdruck (&#8960; 5 Tage)</td>
+											<td>{predictionMap.get(offsetDate(startDate, i + 1))?.featuresUsed.averagePressure5dayMean} hPa</td>
+										</tr>
+										<tr>
+											<td>Temperatur</td>
+											<td>{predictionMap.get(offsetDate(startDate, i + 1))?.featuresUsed.averageTemperature} &deg;C</td>
+										</tr>
+										<tr>
+											<td>Mondphase</td>
+											<td>
+												{#if predictionMap.get(offsetDate(startDate, i + 1))?.featuresUsed?.moonPhase !== undefined}
+													{#if getMoonIcon(predictionMap.get(offsetDate(startDate, i + 1))?.featuresUsed?.moonPhase)}
+														<svelte:component
+															this={getMoonIcon(predictionMap.get(offsetDate(startDate, i + 1))?.featuresUsed?.moonPhase)}
+															width="30"
+															height="30" />
+													{:else}
+														<span>No data</span>
+													{/if}
+												{:else}
+													<span>No data</span>
+												{/if}
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+						<div class="grow flex flex-col rounded-lg content-between p-5 pl-10 {predictionColor(roundedPrediction(offsetDate(startDate, i + 1)))}" data-name="right">
+							<div class="flex flex-row justify-start gap-4" data-name="prediction">
+								<div class="flex flex-col justify-start" data-name="prediction-date">
+									<div class="text-xl font-bold">
+										{dateText(offsetDate(startDate, i + 1)).split(',')[0]}
+									</div>
+									<div class="text-m">
+										{dateText(offsetDate(startDate, i + 1)).split(',')[1]}
+									</div>
+								</div>
+								<div data-name="prediction-value">
+									<div class="text-xl font-bold">
+										{roundedPrediction(offsetDate(startDate, i + 1))} 
+									</div>
+									<div class="text-m">
+										Personen werden heute erwartet
+									</div>
+								</div>
+							</div>
+							<div class="divider w-96"></div>
+							<div class="flex flex-col content-between" data-name="team">
+								<div class="mb-5" data-name="team-title">
+									<h1 class="font-bold text-xl">Triageteam</h1>
+								</div>
+								<div class="flex flex-row justify-start gap-4" data-name="team-cards">
+									{#each getRandomEmployees(teamSize(roundedPrediction(offsetDate(startDate, i + 1)))) as employee}
+										<div class="card bg-base-100 w-32 shadow-xl">
+											<figure class="h-32">
+												<img
+													class="object-cover h-full w-full"
+													src="{employee.imagePath}"
+													alt="{employee.name}" />
+											</figure>
+											<div class="card-body px-5 py-2">
+												<p>{employee.firstName} {employee.lastName}</p>
+											</div>
+										</div>
+									{/each}
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="modal-action">
+						<button class="btn" on:click={() => closeModal(i)}>Close</button>
+					</div>
+				</div>
+			</dialog>
 		{/each}
+
 	</div>
 </div>
 {/if}
